b3891c4d2b965fde6cca67582c9cfa0c6bb77a62
Jeswin Jose <shield178@gmail.com>
2020-12-13 01:35:34 +0800

    Commit 1-Landing page done

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,9 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY =  '4d44410f0c85a223cd2f7babe6d329b4';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/';
+
