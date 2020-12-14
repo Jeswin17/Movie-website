8f556161dc95e1c35ab991edf2256c8c5294b418
Jeswin Jose <shield178@gmail.com>
2020-12-15 03:37:01 +0800

    Favorite state- stage2

--- client/src/components/views/MovieDetailPage/Sections/Favorite.js
+++ client/src/components/views/MovieDetailPage/Sections/Favorite.js
@@ -6,15 +6,15 @@ function Favorite(props){
 
     const [FavoriteNumber , setFavoriteNumber] = useState(0)
     const [ Favorited , setFavorited] = useState(false)
-    useEffect(() =>{
 
-        const variable  = {
-            userFrom: props.userFrom,
-            movieId: props.movieId,
-            movieTitle: props.movieInfo.original_title,
-            movieImage: props.movieInfo.backdrop_path,
-            movieRunTime: props.movieInfo.runtime
-        }
+    const variable  = {
+        userFrom: props.userFrom,
+        movieId: props.movieId,
+        movieTitle: props.movieInfo.original_title,
+        movieImage: props.movieInfo.backdrop_path,
+        movieRunTime: props.movieInfo.runtime
+    }
+    useEffect(() =>{
 
         axios.post('/api/favorite/favoriteNumber', variable)
             .then(response =>{
@@ -35,6 +35,40 @@ function Favorite(props){
                 }
             })
     }, [])
+
+    const onClickFavorite = () => {
+        if(Favorited) {
+// if already aDDED
+                axios.post('/api/favorite/removeFromFavorite', variable)
+                .then(response => {
+                    if(response.data.success){
+                        setFavoriteNumber( FavoriteNumber - 1)
+                        setFavorited(!Favorited)
+                }
+                else{
+                    alert('Failed to remove from  Favorite')
+
+                }
+
+                })
+        } else {
+
+            axios.post('/api/favorite/addToFavorite', variable)
+            .then(response => {
+                if(response.data.success){
+                    setFavoriteNumber( FavoriteNumber + 1)
+                    setFavorited(!Favorited)
+            }
+            else{
+                alert('Failed to get the Favorite')
+
+            }
+
+            })
+        }
+    }
+
+
     return(
         <div>
              <button >{Favorited ? "remove from favorite " : "add to favorite"}{FavoriteNumber}</button>
