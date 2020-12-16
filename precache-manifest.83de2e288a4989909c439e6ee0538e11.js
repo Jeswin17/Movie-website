10e8fbdba59dc1fd31ce7c0c042e7ecfd2862b48
Jeswin Jose <shield178@gmail.com>
2020-12-16 11:34:18 +0800

    Updates

---
+++ precache-manifest.83de2e288a4989909c439e6ee0538e11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c42afacba503ff27d056347b69d096",
+    "url": "/Movie-website/index.html"
+  },
+  {
+    "revision": "1c6bd41cf389b65216b1",
+    "url": "/Movie-website/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "832632fc459b74050d26",
+    "url": "/Movie-website/static/css/main.a1ead8a2.chunk.css"
+  },
+  {
+    "revision": "1c6bd41cf389b65216b1",
+    "url": "/Movie-website/static/js/2.2d258775.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/Movie-website/static/js/2.2d258775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832632fc459b74050d26",
+    "url": "/Movie-website/static/js/main.54e7be9a.chunk.js"
+  },
+  {
+    "revision": "05437249c931716a5fd2",
+    "url": "/Movie-website/static/js/runtime-main.fb6af1a4.js"
+  }
+]);
